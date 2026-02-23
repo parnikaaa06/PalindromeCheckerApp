7fe190ce3a35e2882f1569c77dc38f13b795383e
Parnika Puranik <puranikparnika06@gmail.com>
2026-02-23 16:46:12 +0530

    UC3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,17 +1,17 @@
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        String input = "madam";
-        String reversed = "";
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
-        }
-
-        if (input.equals(reversed)) {
-            System.out.println("Result: " + input + " is a Palindrome.");
-        } else {
-            System.out.println("Result: " + input + " is NOT a Palindrome.");
-        }
+    String input = "madam";
+    String reversed = "";
+    for (int i = input.length() - 1; i >= 0; i--) {
+      reversed = reversed + input.charAt(i);
     }
+
+    if (input.equals(reversed)) {
+      System.out.println("Result: " + input + " is a Palindrome.");
+    } else {
+      System.out.println("Result: " + input + " is NOT a Palindrome.");
+    }
+  }
 }
