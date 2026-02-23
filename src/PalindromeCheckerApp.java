e3177e850e7e0e44e7ea6dcdcb5fe360e0bbae36
Parnika Puranik <puranikparnika06@gmail.com>
2026-02-23 15:27:29 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,25 @@
 public class PalindromeCheckerApp {
 
-  public   static void main(String[] args) {
-      System.out.println("Welcome to the Palindrome Checker Management System");
-      System.out.println("Version : 1.0");
-      System.out.println("System initialized successfully.");
+  public static void main(String[] args) {
+
+
+    String input = "madam";
+
+    // Assume it is palindrome
+    boolean isPalindrome = true;
+
+
+    for (int i = 0; i < input.length() / 2; i++) {
+
+      // Compare characters from both ends
+      if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+        isPalindrome = false;
+        break;
+      }
     }
+
+
+    System.out.println("Input text: " + input);
+    System.out.println("Is it a Palindrome? : " + isPalindrome);
+  }
 }
