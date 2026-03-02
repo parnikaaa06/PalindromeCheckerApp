a71e0acf64def96650509aadd06f068ee9de2677
Parnika Puranik <puranikparnika06@gmail.com>
2026-03-02 14:49:58 +0530

    UC8:Linked List Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,28 +1,30 @@
+import java.util.LinkedList;
+
 public class PalindromeCheckerApp {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    String input = "madam";
+        String input = "level";
 
-    boolean isPalindrome = true;
-    for (int i = 0; i < input.length() / 2; i++) {
-      if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-        isPalindrome = false;
-        break;
-      }
+        LinkedList<Character> list = new LinkedList<>();
+
+        for (char c : input.toCharArray()) {
+            list.add(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (list.size() > 1) {
+            char first = list.removeFirst();
+            char last = list.removeLast();
+
+            if (first != last) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
-
-    System.out.println("Input text: " + input);
-    System.out.println("Is it a Palindrome? : " + isPalindrome);
-    String reversed = "";
-    for (int i = input.length() - 1; i >= 0; i--) {
-      reversed = reversed + input.charAt(i);
-    }
-
-    if (input.equals(reversed)) {
-      System.out.println("Result: " + input + " is a Palindrome.");
-    } else {
-      System.out.println("Result: " + input + " is NOT a Palindrome.");
-    }
-  }
 }
